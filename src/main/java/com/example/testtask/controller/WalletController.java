c012aba846e15fbd3005f5fd3243a1a3fa434d46
Nastya B <2murka2@gmail.com>
2025-06-11 17:09:26 +0300

    add validation

--- src/main/java/com/example/testtask/controller/WalletController.java
+++ src/main/java/com/example/testtask/controller/WalletController.java
@@ -2,6 +2,7 @@ package com.example.testtask.controller;
 
 import com.example.testtask.dto.TransferRequest;
 import com.example.testtask.service.WalletService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,13 +19,13 @@ public class WalletController {
     }
 
     @PostMapping("/wallet")
-    public ResponseEntity<?> doTransfer(@RequestBody TransferRequest transferRequest) {
+    public ResponseEntity<?> doTransfer(@RequestBody @Valid TransferRequest transferRequest) {
         walletService.doTransfer(transferRequest);
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/wallets")
-    public ResponseEntity<?> getBalance(@PathVariable UUID walletId) {
+    @GetMapping("/wallets/{walletId}")
+    public ResponseEntity<BigDecimal> getBalance(@PathVariable UUID walletId) {
         BigDecimal balance = walletService.getBalance(walletId);
         return ResponseEntity.ok(balance);
     }
