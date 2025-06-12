dfd914cd8aa9fe4d4e4abe47384f3d3ea750f0a2
Nastya B <2murka2@gmail.com>
2025-06-12 22:00:59 +0300

    add logg

--- src/main/java/com/example/testtask/controller/WalletController.java
+++ src/main/java/com/example/testtask/controller/WalletController.java
@@ -1,14 +1,16 @@
 package com.example.testtask.controller;
 
 import com.example.testtask.dto.TransferRequest;
+import com.example.testtask.dto.TransferResponse;
 import com.example.testtask.service.WalletService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.util.UUID;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1")
 public class WalletController {
@@ -21,12 +23,14 @@ public class WalletController {
     @PostMapping("/wallet")
     public ResponseEntity<?> doTransfer(@RequestBody @Valid TransferRequest transferRequest) {
         walletService.doTransfer(transferRequest);
+        log.info("Successful transaction for the amount: {}. Wallet Id: {}", transferRequest.getAmount(), transferRequest.getWalletId());
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/wallets/{walletId}")
-    public ResponseEntity<BigDecimal> getBalance(@PathVariable UUID walletId) {
-        BigDecimal balance = walletService.getBalance(walletId);
+    public ResponseEntity<TransferResponse> getBalance(@PathVariable UUID walletId) {
+        TransferResponse balance = walletService.getBalance(walletId);
+        log.info("The amount: {}. Wallet Id: {}", balance.getAmount(), walletId);
         return ResponseEntity.ok(balance);
     }
 }
--- src/main/java/com/example/testtask/controllerAdvice/ControllerAdvice.java
+++ src/main/java/com/example/testtask/controllerAdvice/ControllerAdvice.java
@@ -1,22 +1,39 @@
 package com.example.testtask.controllerAdvice;
 
+import com.example.testtask.exceptions.DataHasNotUpdateException;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Objects;
+@Slf4j
 @org.springframework.web.bind.annotation.ControllerAdvice
 public class ControllerAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex) {
-        return ResponseEntity.badRequest().body(
-                ex.getFieldError().getDefaultMessage() // "Сумма должна быть положительной"
-        );
+        log.error("Error message: {} ", ex.getMessage());
+        return ResponseEntity.badRequest().body(Objects.requireNonNull(ex.getFieldError()).getDefaultMessage());
     }
 
-    @ExceptionHandler(IllegalStateException.class)
-    public ResponseEntity<?> handleInputException(IllegalStateException ex) {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleInputException(IllegalArgumentException ex) {
+        log.error("Error message: {} ", ex.getMessage());
         return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException ex) {
+        log.error("Error message: {} ", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DataHasNotUpdateException.class)
+    public ResponseEntity<?> handleDataHasNotUpdateException(DataHasNotUpdateException ex) {
+        log.error("Error message: {} ", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 }
