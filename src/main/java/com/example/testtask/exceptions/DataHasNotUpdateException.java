01b75fc2b504babf5b3e41796eb673aa7f047090
Nastya B <2murka2@gmail.com>
2025-06-12 22:00:46 +0300

    add exception

---
+++ src/main/java/com/example/testtask/exceptions/DataHasNotUpdateException.java
@@ -0,0 +1,7 @@
+package com.example.testtask.exceptions;
+
+public class DataHasNotUpdateException extends RuntimeException {
+    public DataHasNotUpdateException(String message) {
+        super(message);
+    }
+}
