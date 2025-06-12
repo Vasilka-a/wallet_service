cd816a48bb3359518ff95c9cfc80008a0309a9ef
Nastya B <2murka2@gmail.com>
2025-06-12 22:00:22 +0300

    fix repository

--- src/main/java/com/example/testtask/repository/WalletRepository.java
+++ src/main/java/com/example/testtask/repository/WalletRepository.java
@@ -1,6 +1,7 @@
 package com.example.testtask.repository;
 
 import com.example.testtask.entity.Wallet;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,12 +16,9 @@ public interface WalletRepository extends JpaRepository<Wallet, UUID> {
     @Query("SELECT w.amount FROM Wallet w WHERE w.walletId = :walletId")
     Optional<BigDecimal> getWalletByUUID(UUID walletId);
 
+    @Transactional
     @Modifying
     @Query("UPDATE Wallet w SET w.amount = :actualAmount where w.walletId = :walletId ")
-    int makeDeposit(BigDecimal actualAmount, UUID walletId);
-
-    @Modifying
-    @Query("UPDATE Wallet w SET w.amount = :actualAmount where w.walletId = :walletId ")
-    int makeWithdraw(BigDecimal actualAmount, UUID walletId);
+    int updateAmountById(BigDecimal actualAmount, UUID walletId);
 }
 
